f34d86a2e49ffba408b1c2ae867cfeca79f3dbbb
Saksorn <boat_saksorn@icloud.com>
2025-08-02 13:03:17 +0000

    โครงสร้าง

--- client/src/lib/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // สำคัญ! ให้ส่ง cookie (JWT) ไปด้วย
-});
-
-export default API;
