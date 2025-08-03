14a916051a84d634d0b709086e65dca238d01868
Saksorn <boat_saksorn@icloud.com>
2025-08-03 00:18:36 +0000

    Debug }

--- mail.js
+++ mail.js
@@ -1,18 +1,20 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv"; dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
-export const mailOptions = (to, otp) => {return {
-  from: '"PortSnap" <no-reply@portsnap.com>',
-  to,
-  subject: "Your OTP Code",
-  html: `
+export const mailOptions = (to, otp) => {
+  return {
+    from: '"PortSnap" <no-reply@portsnap.com>',
+    to,
+    subject: "Your OTP Code",
+    html: `
     <p>Your OTP is:</p>
     <p><strong>${otp}</strong></p>
     <form>
@@ -20,18 +22,20 @@ export const mailOptions = (to, otp) => {return {
     </form>
     <p>This code will expire in 10 minutes.</p>
   `,
-}}
-export const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
+  };
+};
+export const generateOTP = () =>
+  Math.floor(100000 + Math.random() * 900000).toString();
 export async function sendOTP(to, otp) {
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to,
     subject: "Your OTP Code",
-    html: `<p>Your OTP is <b>${otp}</b>, valid 5 นาที</p>`
+    html: `<p>Your OTP is <b>${otp}</b>, valid 5 นาที</p>`,
   });
 }
-export async function resetOTP(email,otp) {
-   await transporter.sendMail(mailOptions(email,otp))
+export async function resetOTP(email, otp) {
+  await transporter.sendMail(mailOptions(email, otp));
 }
 
 export async function sendReset(to, link) {
@@ -39,6 +43,6 @@ export async function sendReset(to, link) {
     from: process.env.EMAIL_USER,
     to,
     subject: "Reset your password",
-    html: `<p>คลิกลิงก์เพื่อ reset: <a href="${link}">${link}</a></p>`
+    html: `<p>คลิกลิงก์เพื่อ reset: <a href="${link}">${link}</a></p>`,
   });
 }
